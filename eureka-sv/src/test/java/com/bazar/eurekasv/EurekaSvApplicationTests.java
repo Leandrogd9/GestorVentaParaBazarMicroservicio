a2043ce7507852bbf17013619e7ff5e970c4e0d5
Leandrogd9 <leandrogd67@gmail.com>
2024-03-27 10:18:44 -0300

    creacion eureka-sv

---
+++ eureka-sv/src/test/java/com/bazar/eurekasv/EurekaSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazar.eurekasv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
