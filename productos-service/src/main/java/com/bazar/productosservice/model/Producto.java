224f1dd9f00e73525f7228700fcbd1afc2c5a8cf
Leandrogd9 <leandrogd67@gmail.com>
2024-04-11 13:13:26 -0300

    Implementando microservicio ventas

--- productos-service/src/main/java/com/bazar/productosservice/model/Producto.java
+++ productos-service/src/main/java/com/bazar/productosservice/model/Producto.java
@@ -1,4 +1,29 @@
 package com.bazar.productosservice.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long codigo_producto;
+    @Column(nullable = false)
+    @NotEmpty(message = "no puede estar vacio.")
+    private String nombre;
+    @Column(nullable = false)
+    @NotEmpty(message = "no puede estar vacio.")
+    private String marca;
+    @Column(nullable = false)
+    @NotEmpty(message = "no puede estar vacio.")
+    private Double costo;
+    @Column(nullable = false)
+    @NotEmpty(message = "no puede estar vacio.")
+    private Integer cantidad_disponible;
 }
