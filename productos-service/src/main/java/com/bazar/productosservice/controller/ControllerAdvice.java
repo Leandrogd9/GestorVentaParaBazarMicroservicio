da8893592d1d3119d9eece92b192a6f02d2526cf
Leandrogd9 <leandrogd67@gmail.com>
2024-05-14 18:06:41 -0300

    Implementacion de excepciones perzonalizadas terminadas

--- productos-service/src/main/java/com/bazar/productosservice/controller/ControllerAdvice.java
+++ productos-service/src/main/java/com/bazar/productosservice/controller/ControllerAdvice.java
@@ -1,10 +1,13 @@
 package com.bazar.productosservice.controller;
 
 import com.bazar.productosservice.dto.CustomErrorResponse;
+import com.bazar.productosservice.exception.CheckExistenceException;
+import com.bazar.productosservice.exception.RequestException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +25,34 @@ public class ControllerAdvice {
                 .path(request.getRequestURI())
                 .build();
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<CustomErrorResponse> runtimeExceptionHandler(HttpMessageNotReadableException ex, HttpServletRequest request){
+        CustomErrorResponse error = CustomErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error("Bad Request")
+                .message("Error al deserializar el JSON: " + ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RequestException.class)
+    public ResponseEntity runtimeExceptionHandler(RequestException ex){
+        return new ResponseEntity<>(ex.getMessagesList(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CheckExistenceException.class)
+    public ResponseEntity<CustomErrorResponse> runtimeExceptionHandler(CheckExistenceException ex, HttpServletRequest request){
+        CustomErrorResponse error = CustomErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.value())
+                .error("Not Found")
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
--- ventas-service/src/main/java/com/bazar/ventasservice/controller/ControllerAdvice.java
+++ ventas-service/src/main/java/com/bazar/ventasservice/controller/ControllerAdvice.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,6 +27,18 @@ public class ControllerAdvice {
                 .path(request.getRequestURI())
                 .build();
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<CustomErrorResponse> runtimeExceptionHandler(HttpMessageNotReadableException ex, HttpServletRequest request){
+        CustomErrorResponse error = CustomErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error("Bad Request")
+                .message("Error al deserializar el JSON: " + ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RequestException.class)
