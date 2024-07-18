6f8e3faf412fb2010442574d11c5cdecd3dd9b75
Leandrogd9 <leandrogd67@gmail.com>
2024-07-18 09:55:00 -0300

    Implementando seguridad

--- auth-service/src/main/java/com/bazar/authservice/security/JwtProvider.java
+++ auth-service/src/main/java/com/bazar/authservice/security/JwtProvider.java
@@ -3,9 +3,13 @@ package com.bazar.authservice.security;
 import com.bazar.authservice.model.AuthUser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Base64;
@@ -18,26 +22,24 @@ public class JwtProvider {
 
     @PostConstruct
     protected void init(){
+        System.out.println("Esto es una prueba "+secret);
         secret = Base64.getEncoder().encodeToString(secret.getBytes());
+        System.out.println("Esto es una prueba "+secret);
     }
 
     public String createToken(AuthUser authUser){
-        Instant issuedAt = Instant.now();
-        Instant expirationTime  = issuedAt.plus(Duration.ofHours(1));
-
         return Jwts.builder()
+                .signWith(this.getPrivateKey(secret))
                 .setSubject(authUser.getUsername())
-                .claim("id", authUser.getId())
-                .setIssuedAt(Date.from(issuedAt))
-                .setExpiration(Date.from(expirationTime))
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 3600*1000))
                 .compact();
 
     }
 
     public Boolean validateToken(String token){
         try {
-            Jwts.parser().setSigningKey(secret).build().parseClaimsJws(token);
+            Jwts.parser().setSigningKey(this.getPrivateKey(secret)).build().parseClaimsJws(token);
             return true;
         }catch(Exception e){
             return false;
@@ -46,9 +48,14 @@ public class JwtProvider {
 
     public String getUsernameFromToken(String token){
         try {
-            return Jwts.parser().setSigningKey(secret).build().parseClaimsJws(token).getBody().getSubject();
+            return Jwts.parser().setSigningKey(this.getPrivateKey(secret)).build().parseClaimsJws(token).getBody().getSubject();
         }catch (Exception e){
             return "bad token";
         }
+    }
+
+    private Key getPrivateKey(String secret){
+        byte[] secretBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(secretBytes);
     }
 }
